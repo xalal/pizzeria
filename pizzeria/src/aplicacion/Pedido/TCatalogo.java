5e79d28c7b075048804c33d8c55edadd94a930c2
Imprenta Laurita <Imprenta Laurita@Desktop8_PC>
2014-07-23 21:24:18 -0500

    Subo actualizacion en tomar pedido, estoy programando el confirmar pedido.

---
+++ pizzeria/src/aplicacion/Pedido/TCatalogo.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aplicacion.Pedido;
+
+/**
+ *
+ * @author Imprenta Laurita
+ */
+public class TCatalogo {
+    
+    private int idcatalogo;
+    private String descripcion;
+    private String imagen;
+
+    public TCatalogo() {
+    }
+
+    public TCatalogo(int idcatalogo, String descripcion, String imagen) {
+        this.idcatalogo = idcatalogo;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+    }
+
+    public int getIdcatalogo() {
+        return idcatalogo;
+    }
+
+    public void setIdcatalogo(int idcatalogo) {
+        this.idcatalogo = idcatalogo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+    
+    
+    
+}
